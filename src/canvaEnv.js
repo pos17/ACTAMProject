49b13ff5028af84601ae063a1df4c8435e02e77f
Stefano <stefano.dona.4@studenti.unipd.it>
2021-11-21 15:43:49 +0100

    including Vue

--- src/canvaEnv.js
+++ src/canvaEnv.js
@@ -1,3 +1,6 @@
+import Vue from "vue";
+
+
 const canvasDiv = document.getElementById('canvas-div');
 const sky = document.getElementById('sky');
 console.log('sky width: '+sky.clientWidth);
@@ -8,7 +11,10 @@ var state = {
 } 
 
 const urls = {
-    mountains: new URL('../assets/BG/Mountains.png', import.meta.url),
+    mountains: [
+        new URL('../assets/BG/Mountains.png', import.meta.url),
+        new  URL('../assets/BG/Mountains2.png', import.meta.url),
+    ],
     grass: new URL('../assets/BG/Grass.png', import.meta.url),
     house: new URL('../assets/HOUSE/Home.png', import.meta.url),
     moon: new URL('../assets/MOON/Moon.png', import.meta.url),
@@ -31,7 +37,7 @@ var assets = {
 
 
 function addImage () {
-    assets.mountains = addImageToCanvasDiv(urls.mountains, {
+    assets.mountains = addImageToCanvasDiv('mountains', urls.mountains[0], {
         class: 'overlay',
         width: '100%',
         left: '0',
@@ -39,7 +45,7 @@ function addImage () {
         zIndex:'+1'
     });
     
-    assets.grass = addImageToCanvasDiv(urls.grass, {
+    assets.grass = addImageToCanvasDiv('grass', urls.grass, {
         class: 'overlay',
         width: '100%',
         left: '0',
@@ -47,7 +53,7 @@ function addImage () {
         zIndex:'+1'
     });
 
-    assets.house = addImageToCanvasDiv(urls.house, {
+    assets.house = addImageToCanvasDiv('house', urls.house, {
         class: 'overlay',
         width: `${49/256*100}%`,
         // width: `${this.clientWidth/sky.clientWidth*100}%`,
@@ -56,7 +62,7 @@ function addImage () {
         zIndex:'+1'
     });
 
-    assets.moon = addImageToCanvasDiv(urls.moon, {
+    assets.moon = addImageToCanvasDiv('moon', urls.moon, {
         class: 'overlay',
         width: `${31/256*100}%`,
         left: '10%',
@@ -64,7 +70,7 @@ function addImage () {
         zIndex:'+1'
     });
 
-    assets.bigStar = addImageToCanvasDiv(urls.bigStar  , {
+    assets.bigStar = addImageToCanvasDiv('big-star', urls.bigStar  , {
         class: 'overlay',
         width: `${7/256*100}%`,
         zIndex:'0',
@@ -78,7 +84,7 @@ function addImage () {
 
 function addStars (num) {
     for (var i=0; i<num; i++) {
-        assets.stars[i] = addImageToCanvasDiv(urls.star, {
+        assets.stars[i] = addImageToCanvasDiv('star', urls.star, {
             class: 'overlay',
             width: `${7/256*100}%`,
             left: `${Math.floor(Math.random()*100)}%`,
@@ -137,10 +143,11 @@ function subStar (star) {
 }
 
 // params = {class, height, width, top, left, display}
-function addImageToCanvasDiv (src, params) {
+function addImageToCanvasDiv (id, src, params) {
     var img = new Image();
     img.src = src
 
+    img.id = id ? id : null;
     img.style.position = 'absolute';
     img.style.display = params.display ? params.display : 'block'
     img.style.zIndex = params.zIndex ? params.zIndex : '0';
@@ -180,20 +187,36 @@ function addImageToCanvasDiv (src, params) {
     return img;
 }
 
-
+let timer;
 document.getElementById("stop").onclick = () => {clearInterval(timer)};
 document.getElementById('tree-M').onclick = addTreeMaj;
 document.getElementById('tree-m').onclick = addTreeMin;
 document.getElementById('tree-A').onclick = addTreeAlt;
 
 
-let timer = setInterval(()=> {
+timer = setInterval(()=> {
     subStar(assets.stars[Math.floor(Math.random()*assets.stars.length)]);
 }, 1000)
 
 window.addEventListener('load', ()=>{
     console.log(assets)
 })
+
+var myMount = new Vue({
+    el: '#mountains',
+
+    data: {
+        red: true,
+    },
+    
+    method: {
+        seeMe: function () {
+            if (this.red) {
+                assets.mountains.src = urls.mountains[1]
+            }
+        }
+    }
+}); 
 
 addImage()
 // initCanvas()
