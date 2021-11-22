5757f7e18f20404daa52826bd44bcffef0dff3d6
Stefano <stefano.dona.4@studenti.unipd.it>
2021-11-22 14:24:48 +0100

    fixed stars blinked

--- src/canvaEnv.js
+++ src/canvaEnv.js
@@ -1,5 +1,3 @@
-import Vue from "vue";
-
 
 const canvasDiv = document.getElementById('canvas-div');
 const sky = document.getElementById('sky');
@@ -7,25 +5,92 @@ console.log('sky width: '+sky.clientWidth);
 
 var state = {
     canvas: {},
-    assets: {}
+    assets: {
+        stars: []
+    }
 } 
 
-const urls = {
-    mountains: [
-        new URL('../assets/BG/Mountains.png', import.meta.url),
-        new  URL('../assets/BG/Mountains2.png', import.meta.url),
-    ],
-    grass: new URL('../assets/BG/Grass.png', import.meta.url),
-    house: new URL('../assets/HOUSE/Home.png', import.meta.url),
-    moon: new URL('../assets/MOON/Moon.png', import.meta.url),
-    star: new URL('../assets/Small Star.png', import.meta.url),
-    bigStar: new URL('../assets/Big Star.png', import.meta.url),
-    treeMaj: new URL('../assets/TREES/Tree Maj.png', import.meta.url),
-    treeMin: new URL('../assets/TREES/Tree Min.png', import.meta.url),
-    treeAlt: new URL('../assets/TREES/Tree Alt.png', import.meta.url),
+const new_assets = {
+    mountains: {
+        url: [
+            new URL('../assets/BG/Mountains.png', import.meta.url),
+            new  URL('../assets/BG/Mountains2.png', import.meta.url),
+        ],
+        style: {
+            class: 'overlay',
+            width: '100%',
+            left: '0',
+            bottom: '0',
+            zIndex:'+1'
+        },
+    },
+
+    grass: {
+        url: new URL('../assets/BG/Grass.png', import.meta.url),
+        style: {
+            class: 'overlay',
+            width: '100%',
+            left: '0',
+            bottom: '0',
+            zIndex:'+1'
+        }
+    },
+
+    house: {
+        url: new URL('../assets/HOUSE/Home.png', import.meta.url),
+        style: {
+            class: 'overlay',
+            width: `${49/256*100}%`,
+            right: '10%',
+            bottom: '10%',
+            zIndex:'+1'
+        }
+    },
+
+    moon: {
+        url: [
+            new URL('../assets/MOON/Moon1.png', import.meta.url),
+            new URL('../assets/MOON/Moon2.png', import.meta.url),
+            new URL('../assets/MOON/Moon3.png', import.meta.url),
+            new URL('../assets/MOON/Moon4.png', import.meta.url),
+            new URL('../assets/MOON/Moon5.png', import.meta.url),
+        ],
+        style: {
+            class: 'overlay',
+            width: `${31/256*100}%`,
+            left: '10%',
+            top: '10%',
+            zIndex:'+1'
+        }
+    },
+
+    star: {
+        url: [new URL('../assets/Small Star.png', import.meta.url), new URL('../assets/Big Star.png', import.meta.url),],
+        style: {
+            class: 'overlay',
+            width: `${7/256*100}%`,
+            left: '0',
+            top: '0',
+            zIndex:'0',
+            display: 'none'
+        }
+    },
+
+    trees: {
+        url: {
+            Maj: new URL('../assets/TREES/Tree Maj.png', import.meta.url),
+            Min: new URL('../assets/TREES/Tree Min.png', import.meta.url),
+            Alt: new URL('../assets/TREES/Tree Alt.png', import.meta.url),
+        },
+        style: {
+            class: 'overlay',
+            width: `${19/256*100}%`,
+            zIndex:'+2'
+        }
+    }  
 }
 
-var assets = {
+/* var assets = {
     MoonUrls: ['../assets/MOON/Moon.png'],
     stars: [],
     trees: {
@@ -33,56 +98,55 @@ var assets = {
         Min: [],
         Alt: [],
     }
-}
+} */
 
 
-function addImage () {
-    assets.mountains = addImageToCanvasDiv('mountains', urls.mountains[0], {
+// function addImage () {
+    /* assets.mountains = addImageToCanvasDiv('mountains', urls.mountains[0], {
         class: 'overlay',
         width: '100%',
         left: '0',
         bottom: '0',
         zIndex:'+1'
-    });
+    }); */
     
-    assets.grass = addImageToCanvasDiv('grass', urls.grass, {
+    /* assets.grass = addImageToCanvasDiv('grass', urls.grass, {
         class: 'overlay',
         width: '100%',
         left: '0',
         bottom: '0',
         zIndex:'+1'
-    });
+    }); */
 
-    assets.house = addImageToCanvasDiv('house', urls.house, {
+    /* assets.house = addImageToCanvasDiv('house', urls.house, {
         class: 'overlay',
         width: `${49/256*100}%`,
-        // width: `${this.clientWidth/sky.clientWidth*100}%`,
         right: '10%',
         bottom: '10%',
         zIndex:'+1'
-    });
+    }); */
 
-    assets.moon = addImageToCanvasDiv('moon', urls.moon, {
+    /* assets.moon = addImageToCanvasDiv('moon', urls.moon, {
         class: 'overlay',
         width: `${31/256*100}%`,
         left: '10%',
         top: '10%',
         zIndex:'+1'
-    });
+    }); */
 
-    assets.bigStar = addImageToCanvasDiv('big-star', urls.bigStar  , {
+    /* assets.bigStar = addImageToCanvasDiv('big-star', urls.bigStar  , {
         class: 'overlay',
         width: `${7/256*100}%`,
         zIndex:'0',
         display: 'none'
-    });
+    }); */
 
 
-    addStars(150)
+/*     addStars(150)
 
-}
+} */
 
-function addStars (num) {
+/* function addStars (num) {
     for (var i=0; i<num; i++) {
         assets.stars[i] = addImageToCanvasDiv('star', urls.star, {
             class: 'overlay',
@@ -128,26 +192,52 @@ function addTreeAlt () {
     })
     assets.trees.Alt.push(img)
     console.log(assets)
-}
+} */
 
 
 function subStar (star) {
     // var width = assets.bigStar.style.width; 
-    assets.bigStar.style.top = star.style.top;
-    assets.bigStar.style.left = star.style.left;
-    // assets.bigStar.style.width = width;
-    assets.bigStar.style.display = 'block';
-    setTimeout(() => {
-        assets.bigStar.style.display = 'none';
-    }, 500);
+    // assets.bigStar.style.top = star.style.top;
+    // assets.bigStar.style.left = star.style.left;
+    // // assets.bigStar.style.width = width;
+    // assets.bigStar.style.display = 'block';
+    // setTimeout(() => {
+    //     assets.bigStar.style.display = 'none';
+    // }, 500);
+    // console.log(state.assets.stars.indexOf(star))
+    star.src = new_assets.star.url[1];
+   
+    setTimeout((star)=>{
+        star.src = new_assets.star.url[0];
+    }, 1100, star)
+}
+
+
+function addImage() {
+    state.assets.mountains = addImageToCanvasDiv(new_assets.mountains.url[0], new_assets.mountains.style);
+    state.assets.grass = addImageToCanvasDiv(new_assets.grass.url, new_assets.grass.style);
+    state.assets.house = addImageToCanvasDiv(new_assets.house.url, new_assets.house.style);
+    state.assets.moon = addImageToCanvasDiv(new_assets.moon.url[0], new_assets.moon.style);
+    addStars(150);
+}
+
+function addStars (num) {
+    for (var i=0; i<num; i++) {
+        state.assets.stars[i] = addImageToCanvasDiv(new_assets.star.url[0], {
+            class: 'overlay',
+            width: `${7/256*100}%`,
+            left: `${Math.floor(Math.random()*95)}%`,
+            top: `${Math.floor(Math.random()*70)}%`,
+            zIndex:'0'
+        });
+    }
 }
 
 // params = {class, height, width, top, left, display}
-function addImageToCanvasDiv (id, src, params) {
+function addImageToCanvasDiv (src, params) {
     var img = new Image();
     img.src = src
 
-    img.id = id ? id : null;
     img.style.position = 'absolute';
     img.style.display = params.display ? params.display : 'block'
     img.style.zIndex = params.zIndex ? params.zIndex : '0';
@@ -189,34 +279,23 @@ function addImageToCanvasDiv (id, src, params) {
 
 let timer;
 document.getElementById("stop").onclick = () => {clearInterval(timer)};
-document.getElementById('tree-M').onclick = addTreeMaj;
-document.getElementById('tree-m').onclick = addTreeMin;
-document.getElementById('tree-A').onclick = addTreeAlt;
+// document.getElementById('tree-M').onclick = addTreeMaj;
+// document.getElementById('tree-m').onclick = addTreeMin;
+// document.getElementById('tree-A').onclick = addTreeAlt;
 
 
 timer = setInterval(()=> {
-    subStar(assets.stars[Math.floor(Math.random()*assets.stars.length)]);
+    subStar(state.assets.stars[Math.floor(Math.random()*state.assets.stars.length)]);
 }, 1000)
 
 window.addEventListener('load', ()=>{
-    console.log(assets)
+    console.log(state)
 })
 
-var myMount = new Vue({
-    el: '#mountains',
 
-    data: {
-        red: true,
-    },
-    
-    method: {
-        seeMe: function () {
-            if (this.red) {
-                assets.mountains.src = urls.mountains[1]
-            }
-        }
-    }
-}); 
+/* state.assets.moon.onclick = (e) => {
+    e.target.src = new_assets.mountains.url[1]
+} */
 
 addImage()
 // initCanvas()
