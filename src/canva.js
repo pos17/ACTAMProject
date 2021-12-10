9b58e5896d0779155e37f15aea4274af785d1de1
Stefano <stefano.dona.4@studenti.unipd.it>
2021-12-10 16:22:23 +0100

    added environment changing

--- src/canva.js
+++ src/canva.js
@@ -1,10 +1,15 @@
 import * as Model from "./index.js"
+import * as Tone from 'tone'
+
+//TODO: create modular structure for various environments
 
 const canvasDiv = document.getElementById('canvas-div');
 // const canva = document.getElementById('main-canvas');
 const sky = document.getElementById('sky');
 
 var factor = 16;
+var channels = ['mountain', 'seaside', 'city']
+
 
 var skySunSet = new Image()
 skySunSet.src = new URL('../assets/BG/Background2.png', import.meta.url)
@@ -14,27 +19,44 @@ skySunSet.style.position = 'absolute';
 skySunSet.style.zIndex = '1'
 canvasDiv.appendChild(skySunSet)
 
+var skyDay = new Image()
+skyDay.src = new URL('../assets/BG/Background3.png', import.meta.url)
+skyDay.classList.add('sky')
+skyDay.style.opacity = '0';
+skyDay.style.position = 'absolute';
+skyDay.style.zIndex = '1'
+canvasDiv.appendChild(skyDay)
+
 var canvas = document.createElement("canvas")
 canvas.className = "canvases";
 canvas.width = 256*factor;
 canvas.height = 128*factor;
 canvasDiv.appendChild(canvas);
+// state.canvas = canvas;
 
 var ctx = canvas.getContext('2d');
-// ctx.imageSmoothingEnabled = false;
 
-var mountains = new Image();
-var grass = new Image();
-var house = new Image();
+var bg = new Image()
+var floor = new Image()
+var building = new Image()
+
 var moon = new Image();
 var sun = new Image();
 var star = new Image();
+
+var time0 = new Date();
+var omega = 0; /* canvas angular speed */
+var moonRadius = canvas.width/1.1;
+var alpha0 = Math.acos((canvas.width/2)/moonRadius) 
+
+var t = Tone.Time('16m').toMilliseconds()
 
 var state = {
     canvas: {},
     assets: {
         stars: []
-    }
+    },
+    environment: channels[0],
 } 
 
 const new_assets = {
@@ -45,8 +67,34 @@ const new_assets = {
         index: 0
     },
 
+    sea: {
+        url: new URL('../assets/BG/Sea.png', import.meta.url),
+        left: 0,
+        bottom: 0,
+        index: 0
+    },
+
+    skyline: {
+        url: new URL('../assets/BG/Skyline.png', import.meta.url),
+        left: 0,
+        bottom: 0,
+        index: 0
+    },
+
     grass: {
         url: new URL('../assets/BG/Grass.png', import.meta.url),
+        left: 0,
+        bottom: 0,
+    },
+
+    sand: {
+        url: new URL('../assets/BG/Sand.png', import.meta.url),
+        left: 0,
+        bottom: 0,
+    },
+
+    concrete: {
+        url: new URL('../assets/BG/Concrete.png', import.meta.url),
         left: 0,
         bottom: 0,
     },
@@ -90,20 +138,49 @@ const new_assets = {
     }  
 }
 
-function initImages(){
-    mountains.src = new_assets.mountains.url;
-    grass.src = new_assets.grass.url;
-    house.src = new_assets.house.url;
+const environment = {
+    mountain: {
+        background: new_assets.mountains,
+        floor: new_assets.grass,
+        building: new_assets.house,
+    },
+    desert: {
+        background: new_assets.desertMountains,
+        floor: {},
+        building: {},
+    },
+    city: {
+        background: new_assets.skyline,
+        floor: new_assets.concrete,
+        building: new_assets.house,
+    },
+    seaside: {
+        background: new_assets.sea,
+        floor: new_assets.sand,
+        building: new_assets.house,
+    }
+}
+
+function initImages(env){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    bg.src = environment[env].background.url
+    floor.src = environment[env].floor.url
+    building.src = environment[env].building.url
+
     moon.src = new_assets.moon.url;
     sun.src = new_assets.sun.url;
     star.src = new_assets.star.url[0];
+
+    state.assets.stars = []
 
     for (i=0; i<250; i++){
         var aStar = {img: star, left: Math.random(), bottom: 1-(Math.random()*0.7)}
         state.assets.stars.push(aStar);
     }
 
-    window.requestAnimationFrame(createEnvironment);
+    console.log(state.assets.stars)
+
+    window.requestAnimationFrame(()=>{createEnvironment(env)});
 }
 
 
@@ -115,7 +192,8 @@ function drawThisImage (img, left, bottom) {
     ctx.drawImage(img, x, y-h, w, h)
 }
 
-function createEnvironment() {
+function createEnvironment(env) {
+    var time = Date.now()
 
     var h = canvas.height;
     var w = canvas.width; 
@@ -128,13 +206,16 @@ function createEnvironment() {
     })
 
     // ANIMATED IMAGES
-    var time = new Date();
+    var s = moonRadius - Math.sqrt(Math.pow(moonRadius,2)-(Math.pow(canvas.width,2)/4))
+    var a = 2 * Math.acos(1-(s/moonRadius))
+    omega = a/t;
+
     ctx.save();
     ctx.translate(w/2, h*2)
-    var angle = (((2 * Math.PI) / 6) * time.getSeconds() + ((2 * Math.PI) / 6000) * time.getMilliseconds())/40
+    // var angle = ((a/60)*time.getSeconds()+(a/60000)*time.getMilliseconds());
+    // var angle = ((2*Math.PI/6000)*time.getSeconds()) + ((2*Math.PI/100000)*time.getMilliseconds());
+    var angle = (alpha0 + omega * (time-time0.getTime()))
     ctx.rotate(angle);
-
-    var moonRadius = w/1.1; 
 
     ctx.save()
     ctx.translate(moonRadius, 0)
@@ -160,31 +241,34 @@ function createEnvironment() {
     ctx.drawImage(sun, 0, 0, sun.naturalWidth*factor, sun.naturalHeight*factor);
     ctx.restore()
 
-    // ctx.drawImage(moon, moonRadius, moonRadius);
-
-    
     ctx.restore()
-
+    
     // STATIC IMAGES
-    drawThisImage(mountains, new_assets.mountains.left, new_assets.mountains.bottom);
-    drawThisImage(grass, new_assets.grass.left, new_assets.grass.bottom);
-    drawThisImage(house, new_assets.house.left, new_assets.house.bottom)
+    drawThisImage(bg, environment[env].background.left, environment[env].background.bottom);
+    drawThisImage(floor, environment[env].floor.left, environment[env].floor.bottom);
+    drawThisImage(building, environment[env].building.left, environment[env].building.bottom);
 
     blendBG()
 
-    window.requestAnimationFrame(createEnvironment);
+    window.requestAnimationFrame(() => {createEnvironment(env)});
 }
 
 function blendBG() {
-    var opa = parseFloat(skySunSet.style.opacity.split()[0])
+    var opaSunSet = parseFloat(skySunSet.style.opacity.split()[0])
+    var opaDay = parseFloat(skyDay.style.opacity.split()[0])
 
-    if (opa<=1){
-        var opac = opa + 0.0005;
+    if (opaSunSet<=1){
+        var opac = opaSunSet + 0.0005;
         skySunSet.style.opacity = `${opac}`
+    } else {
+        if (opaDay<=1){
+            var opac = opaDay + 0.0005;
+            skyDay.style.opacity = `${opac}`
+        }
     }
 }
 
-initImages();  
+initImages(state.environment);  
 
 
 /* -------------------------------------------------------- */
@@ -221,4 +305,12 @@ playButton.onclick = ()=> {
         Model.startMusic(); 
         
         }
+}
+
+document.getElementById('change').onclick = () => {
+    var idx = channels.indexOf(state.environment)
+    idx = (idx + 1) % channels.length
+    state.environment = channels[idx]; 
+    
+    initImages(state.environment)
 }
