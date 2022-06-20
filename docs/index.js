bae41524de8a9ec9fff803a0e5e01d7bf75e0fea
pox17 <paos9904@gmail.com>
2022-06-20 15:47:44 +0200

    started adding guide

--- docs/index.js
+++ docs/index.js
@@ -3389,3 +3389,36 @@ class DrumMachine {
     }
 
 }
+
+
+
+
+/*-----------------------------user guide tour------------------------------------------------------------*/
+
+const tour = new Shepherd.Tour({
+    useModalOverlay: true,
+    defaultStepOptions: {
+      classes: 'shadow-md bg-purple-dark',
+      scrollTo: true
+    }
+  });
+
+  tour.addStep({
+    id: 'example-step',
+    text: 'This step is attached to the bottom of the <code>.example-css-selector</code> element.',
+    attachTo: {
+      element: '.example-css-selector',
+      on: 'bottom'
+    },
+    classes: 'example-step-extra-class',
+    buttons: [
+      {
+        text: 'Next',
+        action: tour.next
+      }
+    ]
+  });
+
+  document.getElementById("guideTourStart").onclick= function() {
+    tour.start();
+  }
